b72fc2e247d3a72331fe5890d64221ab70061523
Egor-U <t3ugeg00@students.oamk.fi>
2024-03-18 16:01:21 +0200

    task 3 done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
+
 const input = document.querySelector('#task');
 input.addEventListener('keypress', (event) => {
     if (event.key == 'Enter') {
         event.preventDefault();
         const task = input.value.trim();
         if (task != '') {
-            renderTask(task);
-            input.value = '';
+            saveTask(task).then((json) => {
+                renderTask(task);
+                input.value = '';
+            })
         }
     }
 })
@@ -32,3 +35,40 @@ function renderTask(task) {
     li.appendChild(label);
     ul.appendChild(li);
 }
+
+const BACKEND_ROOT_URL = 'http://localhost:3001';
+
+const list = document.querySelector('#tasks');
+
+input.disabled = true;
+
+const getTasks = async() => {
+    try {
+        const response = await fetch(BACKEND_ROOT_URL);
+        const json = await response.json();
+        json.forEach(task => {
+            renderTask(task.description);
+        });
+        input.disabled = false;
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+const saveTask = async(task) => {
+    try {
+        const json = JSON.stringify({description: task});
+        const response = await fetch(BACKEND_ROOT_URL + '/new', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        })
+        return response.json();
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+getTasks();
